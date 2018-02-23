a6f63245c9e824efca1c22ed5e9ff05351c63bce
Alex van der Linden <alexvanderlinden@me.com>
2018-02-23 16:03:36 +0100

    Feat: now iterating over list of Computers and Programs

--- CAO_1/main.cpp
+++ CAO_1/main.cpp
@@ -6,46 +6,50 @@
  */
 
 #include <iostream>
+#include <list>
+#include <iterator>
+
 #include "computer.h"
 #include "program.h"
 
 int main()
 {
-	// initialize computers
-	Computer computer1 = Computer(1.0, 2, 2, 3, 4);
-	Computer computer2 = Computer(1.2, 2, 3, 4, 3);
-	Computer computer3 = Computer(2.0, 2, 2, 4, 6);
+	// create two lists that hold Computer and Program objects
+	std::list<Computer> ComputerList;
+	std::list<Program> ProgramList;
 
-	// initialize programs
-	Program programA = Program(2000, 100, 100, 50);
-	Program programB = Program(2000, 0.1, 0.4, 0.25);
-	Program programC = Program(500, 100, 2000, 200);
+	// first computer will be added to first location in the list
+	ComputerList.push_back(Computer(1.0, 2, 2, 3, 4));
+	ComputerList.push_back(Computer(1.2, 2, 3, 4, 3));
+	ComputerList.push_back(Computer(2.0, 2, 2, 4, 6));
 
-	// calculate and print stats for computer1 and programA, B and C.
-	std::cout << "---- computer 1 ----\n"
-		<< computer1 << "\n"
-		<< "Global MIPS Rating\t: " << computer1.calculateMIPS() << "\n"
-		<< "MIPS Program A\t\t: " << computer1.calculateMIPS(programA) << "\n"
-		<< "MIPS Program B\t\t: " << computer1.calculateMIPS(programB) << "\n"
-		<< "MIPS Program C\t\t: " << computer1.calculateMIPS(programC) << "\n"
-		<< "\n\n";
+	// first program will be added to first location in the list
+	ProgramList.push_back(Program(2000, 100, 100, 50));
+	ProgramList.push_back(Program(2000, 0.1, 0.4, 0.25));
+	ProgramList.push_back(Program(500, 100, 2000, 200));
 
-	// calculate and print stats for computer2 and programA, B and C.
-	std::cout << "---- computer 2 ----\n"
-			<< computer2 << "\n"
-			<< "Global MIPS Rating\t: " << computer2.calculateMIPS() << "\n"
-			<< "MIPS Program A\t\t: " << computer2.calculateMIPS(programA) << "\n"
-			<< "MIPS Program B\t\t: " << computer2.calculateMIPS(programB) << "\n"
-			<< "MIPS Program C\t\t: " << computer2.calculateMIPS(programC) << "\n"
-			<< "\n\n";
+	// create iterators for iterating the computer and program lists
+	std::list<Computer>::iterator pc_it;
+	std::list<Program>::iterator prog_it;
 
-	// calculate and print stats for computer3 and programA, B and C.
-	std::cout << "---- computer 3 ----\n"
-			<< computer3 << "\n"
-			<< "Global MIPS Rating\t: " << computer3.calculateMIPS() << "\n"
-			<< "MIPS Program A\t\t: " << computer3.calculateMIPS(programA) << "\n"
-			<< "MIPS Program B\t\t: " << computer3.calculateMIPS(programB) << "\n"
-			<< "MIPS Program C\t\t: " << computer3.calculateMIPS(programC) << "\n"
-			<< "\n\n";
+	// iterate over the Computer objects in the list
+	for (pc_it = ComputerList.begin(); pc_it != ComputerList.end(); ++pc_it)
+	{
+		// calculate and print stats for computer1 and programA, B and C.
+		std::cout << "---- computer "
+			<< std::distance(ComputerList.begin(), pc_it)+1 << " ----\n"
+			<< *pc_it << "\n"
+			<< "Global MIPS Rating\t: " << pc_it->calculateMIPS() << "\n";
+
+		// iterate over Program objects in the list
+		for (prog_it = ProgramList.begin(); prog_it != ProgramList.end(); ++prog_it)
+		{
+			std::cout << "MIPS Program "
+					<< (char) (0x41 + std::distance(ProgramList.begin(), prog_it)) << "\t\t: "
+					<< pc_it->calculateMIPS(*prog_it) << "\n";
+		}
+		std::cout << "\n\n";
+	}
+	return 0;
 }
 
