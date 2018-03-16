c557cc834d9d07f1f1f5dc29b4694db80f66606f
Alex van der Linden <alexvanderlind@tudelft.net>
2018-03-16 17:23:46 +0100

    assignment 1 start with uart

--- CAO_4/main.c
+++ CAO_4/main.c
@@ -1,30 +1,40 @@
 #include "LPC13xx.h"
+#include "delay.h"
+#include "uart.h"
 
-void delay(void);
 
 int main(void)
 {
-	LPC_GPIO0->DIR = (1<<7); //Config PIO0_7 as Output
+	LPC_GPIO3->DIR = (1<<0); //Config PIO0_7 as Output
+
+    char msg[] = { 'H','e','l','l','o',' ','f','r','o','m',' ','L','P','C','1','3','4','3','\0' };
+	int count=0;
+
+	initUART();
+
 
 	while(1)
 	{
-		LPC_GPIO0->DATA = (1<<7); //Drive output HIGH to turn LED ON
+		LPC_GPIO3->DATA = (1<<0); //Drive output HIGH to turn LED ON
 		// Better way would be LPC_GPIO0->DATA |= (1<<7);
 
-		delay();
-		LPC_GPIO0->DATA = 0x0; //Drive output LOW to turn LED OFF
+		delay_ms(500);
+		LPC_GPIO3->DATA = 0x0; //Drive output LOW to turn LED OFF
 		// Better way would be LPC_GPIO0->DATA &= ~(1<<7);
 
-		delay();
+		delay_ms(500);
+
+
+        while( msg[count]!='\0' )
+        {
+            uartWrite(msg[count]);
+            count++;
+        }
+        //Send NEW Line Character(s) i.e. "\n"
+        uartWrite(CARRIAGE_RETURN); //Comment this for Linux or MacOS
+        uartWrite(LINE_FEED); //Windows uses CR+LF for newline.
+        count=0; // reset counter
+
 	}
 	return 0; //normally this wont execute
-}
-
-void delay(void) //Hard-coded delay function
-{
-	int count,i=0;
-	for(count=0; count < 4500000; count++) //You can edit this as per your needs
-	{
-		i++; //something needs to be here else compiler will remove the for loop!
-	}
 }
